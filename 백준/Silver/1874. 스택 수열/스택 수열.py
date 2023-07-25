870e2dae116ba7584a41739a46a7bf5bfc3d59d8
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2023-07-25 18:22:19 +0200

    [Silver II] Title: 스택 수열, Time: 4144 ms, Memory: 36696 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,24 @@
+n=int(input())
+l1=list(int(input()) for _ in range(n)) #입력받기
+a=len(l1)
+stack=[]
+result=[]
+cnt,no=0,0
+
+for i in range(a):
+    for j in range(cnt+1, l1[i]+1):
+        stack.append(j)
+        result.append("+")
+        cnt+=1
+    if cnt>=l1[i]:
+        if l1[i]==stack[-1]:
+            stack.pop()
+            result.append("-")
+        else:
+            no=1
+
+if no==1:
+    print("NO")
+else:
+    for i in result:
+        print(i)
