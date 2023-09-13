00fea490a044af7591483fdf8b19108f0fa687f6
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2023-09-13 18:13:22 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34136 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+n=int(sys.stdin.readline().strip())
+l1=deque([])
+
+for _ in range(n):
+    line=list(sys.stdin.readline().strip().split())
+    cmd=line[0] 
+    if len(line)==2:
+        num=line[1]
+    if cmd=='push':
+        l1.append(num)
+    elif cmd=='pop':
+        if len(l1)==0:
+            print(-1)
+        else:
+            k=l1.popleft()
+            print(k)
+    elif cmd=='front':
+        if len(l1)==0:
+            print(-1)
+        else:
+            print(l1[0])
+    elif cmd=='back':
+        if len(l1)==0:
+            print(-1)
+        else:
+            print(l1[-1])
+    elif cmd=='size':
+        print(len(l1))
+    else:
+        if len(l1)==0:
+            print(1)
+        else:
+            print(0)
