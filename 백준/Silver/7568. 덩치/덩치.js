4d3359fdd9992919e5c7898a0f699ec403df7c7c
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2025-01-13 21:06:21 +0900

    [Silver V] Title: 덩치, Time: 96 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.js
+++ 백준/Silver/7568. 덩치/덩치.js
@@ -1,26 +1,25 @@
-let fs = require('fs');
+// 덩치
 
-const inputs = fs.readFileSync('/dev/stdin').toString().split('\n');
-//const inputs = fs.readFileSync(__dirname + '/ex.txt').toString().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const arr = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const n = Number(inputs[0])
-let l1 = []
+const n = Number(arr[0]);
 
-for (let i=0; i<n; i++) {
-    let [x, y] = inputs[i+1].split(' ').map(Number)
-    l1.push([x, y])
+let l1 = new Array(n).fill(1);
+let p = [];
+
+for (let i = 1; i <= n; i++) {
+  const [x, y] = arr[i].split(" ").map(Number);
+  p.push([x, y]);
 }
 
-let score = new Array(l1.length).fill(1)
-
-for (let i=0; i<n; i++) {
-    for (let j=0; j<n; j++){
-        if (l1[i][0]<l1[j][0]) {
-            if (l1[i][1]<l1[j][1]) {
-                score[i]+=1
-            }
-        }
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    if (p[i][0] > p[j][0] && p[i][1] > p[j][1]) {
+      l1[j]++;
     }
+  }
 }
 
-console.log(score.join(' '))
+console.log(l1.join(" "));
