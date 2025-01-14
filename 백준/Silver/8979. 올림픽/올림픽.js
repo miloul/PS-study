030beae8c215fd8b4bd5a17d4472add0e3df1f24
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2025-01-14 19:28:41 +0900

    [Silver V] Title: 올림픽, Time: 156 ms, Memory: 10480 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.js
+++ 백준/Silver/8979. 올림픽/올림픽.js
@@ -6,33 +6,38 @@ const arr = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [n, k] = arr[0].split(" ").map(Number);
 
-let m = new Array(n + 1).fill([]);
+let m = [];
 for (let i = 1; i <= n; i++) {
   let l1 = arr[i].split(" ").map(Number);
-  m[l1[0]] = [l1[1], l1[2], l1[3]];
+  m.push(l1);
 }
 
-let rank = new Array(n + 1).fill(0);
+m.sort((a, b) => {
+  if (a[1] !== b[1]) {
+    return b[1] - a[1];
+  }
+  if (a[2] !== b[2]) {
+    return b[2] - a[2];
+  }
+  return b[3] - a[3];
+});
 
-const compare = (a, b) => {
-  return a < b ? 1 : 0;
-};
+let rank = 1;
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    const gold = compare(m[i][0], m[j][0]);
-    const silver = compare(m[i][1], m[j][1]);
-    const cu = compare(m[i][2], m[j][2]);
-    if (gold) {
-      rank[i]++;
-    }
-    if (silver && !gold) {
-      rank[i]++;
-    }
-    if (cu && !silver && !gold) {
-      rank[i]++;
+if (m[0][0] === k) {
+  console.log(rank);
+} else {
+  for (let i = 1; i < n; i++) {
+    const [prev, pg, ps, pc] = m[i - 1];
+    const [cur, cg, cs, cc] = m[i];
+
+    if (pg > cg) rank = i + 1;
+    else if (ps > cs) rank = i + 1;
+    else if (pc > cc) rank = i + 1;
+
+    if (cur === k) {
+      console.log(rank);
+      break;
     }
   }
 }
-
-console.log(rank[k]);
