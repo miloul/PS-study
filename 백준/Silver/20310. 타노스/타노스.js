29a49009b46f191a5649e03406bf3e25046519ce
Seung Min Lee <milouw56@gmail.com>
2025-02-01 12:22:09 +0900

    [Silver III] Title: 타노스, Time: 100 ms, Memory: 9384 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.js
+++ 백준/Silver/20310. 타노스/타노스.js
@@ -1,19 +1,29 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const arr = fs.readFileSync(filePath).toString().trim().split("").map(Number);
+let arr = fs.readFileSync(filePath).toString().trim().split("").map(Number);
 
 let cnt0 = (cnt1 = 0);
 
 for (const s of arr) {
   s === 0 ? cnt0++ : cnt1++;
 }
+const cnt0h = cnt0 / 2;
+const cnt1h = cnt1 / 2;
 
-let answer = [];
-for (let i = 0; i < cnt0; i = i + 2) {
-  answer.push(0);
-}
-for (let i = 0; i < cnt1; i = i + 2) {
-  answer.push(1);
+for (let i = arr.length - 1; i > 0; i--) {
+  if (arr[i] === 0) {
+    arr[i] = "";
+    cnt0--;
+  }
+  if (cnt0 === cnt0h) break;
 }
 
-console.log(answer.join(""));
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === 1) {
+    arr[i] = "";
+    cnt1--;
+  }
+
+  if (cnt1 === cnt1h) break;
+}
+console.log(arr.join(""));
