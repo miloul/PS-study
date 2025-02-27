18930a7759dab192aed2a81e60927d579a0847ff
Seung Min Lee <milouw56@gmail.com>
2025-02-27 09:44:06 +0900

    [Gold V] Title: ABCDE, Time: 248 ms, Memory: 12356 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.js
@@ -0,0 +1,38 @@
+// ABCDE
+// depth 5
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+
+let graph = Array.from(new Array(n + 1), () => new Array());
+let visited = new Array(n + 1).fill(0);
+
+for (let i = 1; i <= m; i++) {
+  const [x, y] = input[i].split(" ").map(Number);
+  graph[x].push(y);
+  graph[y].push(x);
+}
+
+let result = 0;
+
+const dfs = (node, depth) => {
+  visited[node] = 1;
+  if (result) return;
+  if (depth === 5) {
+    result = 1;
+    return;
+  }
+  for (const g of graph[node]) {
+    if (!visited[g]) dfs(g, depth + 1);
+  }
+  visited[node] = 0;
+};
+
+for (let i = 0; i < n; i++) {
+  if (result) break;
+  dfs(i, 1);
+}
+
+console.log(result);
