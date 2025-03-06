658274ca0bc6f0b69cfde106b00936cf305984de
Seung Min Lee <milouw56@gmail.com>
2025-03-06 10:53:15 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -2,18 +2,17 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const inputs = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let [n, k] = inputs[0].split(" ").map(Number);
-
-function solution(list) {
-  let cnt = 0;
-  for (let i = 0; i < n; i++) {
-    let coin = list[i];
-    if (k >= coin) {
-      cnt += parseInt(k / coin);
-      k = k % coin;
+function solution(coins, target) {
+    let cnt = 0
+    for (const coin of coins) {
+        if (target >= coin) {
+            cnt += Math.floor(target/coin)
+            target = target % coin
+        }
     }
-  }
-  return cnt;
+    return cnt
 }
 
-console.log(solution(inputs.slice(1).reverse().map(Number)));
+let [n, k] = inputs.shift().split(' ').map(Number)
+let list = inputs.reverse().map(Number)
+console.log(solution(list, k))
