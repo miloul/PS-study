23f7b6ca7566aa56441404bdbd33aa0ad25b8686
이승민 Seung Min Lee <milouw56@gmail.com>
2026-03-14 16:54:47 +0900

    [Silver III] Title: N과 M (3), Time: 732 ms, Memory: 149364 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.js
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -4,22 +4,17 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [n, m] = input[0].split(" ").map(Number);
 
-let visited = new Array(n+1).fill(0);
-
 let result = []
 // 백트래킹..
 const dfs = (depth, nums) => {
     if (depth === m) {
-        result.push(nums.join(" "))
+        result.push(nums.join(" "));
         return;
     }
     for (let i=0; i<n; i++) {
-        if (visited[i]==0) { // 방문안했으면
-            nums.push(i+1)
-            dfs(depth+1, nums);
-            nums.pop();
-            visited[i]=0;
-        }
+        nums.push(i+1)
+        dfs(depth+1, nums);
+        nums.pop();
     }
 }
 
